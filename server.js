6619938eb9283f6b0364032c87712ad0bf9a8cff
LoganHowat <lhowat624@hotmail.com>
2023-02-20 14:34:10 +0000

    Finished task

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const port = 3000;
 
 //TODO
 
+app.get('/musicians',async (request,response) =>{
+    let data = await Musician.findAll();
+    response.send(JSON.stringify(data));
+})
+
 app.listen(port, () => {
     sequelize.sync();
     console.log(`Listening on port ${port}`)
